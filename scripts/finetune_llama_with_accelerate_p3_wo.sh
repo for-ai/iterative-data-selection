05003a0c8764fc7479038727c4aec72c2f4556fb
Simon <simon011130@gmail.com>
2023-12-27 14:38:31 +0000

    add kmeanscenter selection idx

--- scripts/finetune_llama_with_accelerate_p3_wo.sh
+++ scripts/finetune_llama_with_accelerate_p3_wo.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=0
 MODEL_SIZE=7B
 NUM_GPUS=1
 BATCH_SIZE_PER_GPU=4
-EVAL_BATCH_SIZE_PER_GPU=32
+EVAL_BATCH_SIZE_PER_GPU=64
 TOTAL_BATCH_SIZE=64
 MODEL_NAME_OR_PATH=meta-llama/Llama-2-7b-hf
 # MODEL_NAME_OR_PATH=/mnt/data/data-selection/output/data_selection_Llama-2-7b-hf-sharegpt_lora_merged_step_2000
@@ -39,6 +39,8 @@ python3 \
     --low_cpu_mem_usage \
     --do_eval \
     --output_dir output/data_selection_${MODEL_NAME}_lora \
+    --selection_indices /mnt/ceph_rbd/data-selection/selection/indices/kmeansrandom_0.1.pkl \
+    --eval_batch_size $EVAL_BATCH_SIZE_PER_GPU \
     --with_tracking \
     --logging_steps 10 \
     --report_to wandb
