b0fea23b1cda0c98658eced707f7db58c33b0cd6
Simon <simon011130@gmail.com>
2024-01-14 22:29:04 +0000

    add for unified encoder class

---
+++ selection/encoder/base.py
@@ -0,0 +1,12 @@
+import abc
+
+import torch
+
+# Create an encoder class which is abstract
+class Encoder(abc.ABC):
+    def __init__(self, config):
+        self.config = config
+
+    @abc.abstractmethod
+    def encode(self, example):
+        pass
