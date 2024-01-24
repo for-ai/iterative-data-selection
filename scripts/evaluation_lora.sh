693c01c0ab5faf887c3c8e703ec6194a69a05650
simonucl <simon011130@gmail.com>
2024-01-24 22:13:43 +0000

    update the scripts

--- scripts/evaluation_lora.sh
+++ scripts/evaluation_lora.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 HF_TOKEN=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
 
 MODEL_SIZE=7B
@@ -12,20 +12,20 @@ TRAIN_FILE=data/processed/sharegpt/sharegpt_data.jsonl
 STEP_NAME=""
 EPOCH_NAME=""
 
-MODEL_NAME=Llama-2-7b-hf-sharegpt-KCenterGreedyDeita-005-lora-lr-5e-5-epoch-4
+MODEL_NAME=Llama-2-7b-hf-sharegpt-KCenterGreedyDeita-005-lora-lr-2e-4-epoch-9
 
-# for peft in simonycl/data-selection-Llama-2-7b-sharegpt-KCenterGreedyDeita-0.05-lora
-# do
-#     PEFT_PATH=$peft
-#     # extract the model name from split string by delimiter '-' and get the third last element
-#     python3 -m finetune.merge_lora \
-#         --lora_model_name_or_path $PEFT_PATH \
-#         --base_model_name_or_path $MODEL_NAME_OR_PATH \
-#         --tokenizer_name_or_path $MODEL_NAME_OR_PATH \
-#         --output_dir /mnt/data/data-selection/output/data_selection_${MODEL_NAME} \
-#         --save_tokenizer \
-#         --push_to_hub_id simonycl/data_selection_${MODEL_NAME}
-# done
+for peft in simonycl/data-selection-Llama-2-7b-sharegpt-KCenterGreedyDeita-0.05-lora-lr-2e-4
+do
+    PEFT_PATH=$peft
+    # extract the model name from split string by delimiter '-' and get the third last element
+    python3 -m finetune.merge_lora \
+        --lora_model_name_or_path $PEFT_PATH \
+        --base_model_name_or_path $MODEL_NAME_OR_PATH \
+        --tokenizer_name_or_path $MODEL_NAME_OR_PATH \
+        --output_dir /mnt/data/data-selection/output/data_selection_${MODEL_NAME} \
+        --save_tokenizer \
+        --push_to_hub_id simonycl/data_selection_${MODEL_NAME}
+done
 
 cd /mnt/data/lm-evaluation-harness
 bash eval_model.sh /mnt/data/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/data/EasyLM/eval_results/data_selection_$MODEL_NAME.log
