633c749ee594b73fa3adc38927f4647b2a1eb912
Simon <simon011130@gmail.com>
2024-01-25 14:16:31 +0000

    push for remote eval

--- scripts/evaluation_lora.sh
+++ scripts/evaluation_lora.sh
@@ -12,24 +12,23 @@ TRAIN_FILE=data/processed/sharegpt/sharegpt_data.jsonl
 STEP_NAME=""
 EPOCH_NAME=""
 
-MODEL_NAME=Llama-2-7b-hf-sharegpt-KCenterGreedyDeita-005-lora-lr-5e-5-epoch-4
+MODEL_NAME=Llama-2-7b-hf-sharegpt-KMenasRandomDeita-1024-005-lora-epoch_4
 
-# for peft in simonycl/data-selection-Llama-2-7b-sharegpt-KCenterGreedyDeita-0.05-lora
-# do
-#     PEFT_PATH=$peft
-#     # extract the model name from split string by delimiter '-' and get the third last element
-#     python3 -m finetune.merge_lora \
-#         --lora_model_name_or_path $PEFT_PATH \
-#         --base_model_name_or_path $MODEL_NAME_OR_PATH \
-#         --tokenizer_name_or_path $MODEL_NAME_OR_PATH \
-#         --output_dir /mnt/data/data-selection/output/data_selection_${MODEL_NAME} \
-#         --save_tokenizer \
-#         --push_to_hub_id simonycl/data_selection_${MODEL_NAME}
-# done
+for peft in simonycl/data-selection-Llama-2-7b-sharegpt-KMenasRandomDeita-1024-0.05-lora-epoch_4
+do
+    PEFT_PATH=$peft
+    # extract the model name from split string by delimiter '-' and get the third last element
+    python3 -m finetune.merge_lora \
+        --lora_model_name_or_path $PEFT_PATH \
+        --base_model_name_or_path $MODEL_NAME_OR_PATH \
+        --tokenizer_name_or_path $MODEL_NAME_OR_PATH \
+        --output_dir /mnt/data/data-selection/output/data_selection_${MODEL_NAME} \
+        --save_tokenizer \
+        --push_to_hub_id simonycl/data_selection_${MODEL_NAME}
+done
 
 cd /mnt/data/lm-evaluation-harness
 bash eval_model.sh /mnt/data/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/data/EasyLM/eval_results/data_selection_$MODEL_NAME.log
-# bash eval_model.sh simonycl/llama-2-7b-hf-sharegpt-full-ft-$EPOCH Llama-2-7b-hf-sharegpt-$EPOCH 
 
 cd /mnt/data/data-selection/
 bash scripts/eval/cohere.sh /mnt/data/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/data/EasyLM/eval_results/data_selection_$MODEL_NAME-mmlu.log
