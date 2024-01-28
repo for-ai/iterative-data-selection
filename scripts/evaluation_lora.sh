f0999fdec85425b5b04e77dfeb427e927aa854b9
simon <simon011130@gmail.com>
2024-01-28 00:42:15 +0000

    add evaluation

--- scripts/evaluation_lora.sh
+++ scripts/evaluation_lora.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 HF_TOKEN=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
 
 MODEL_SIZE=7B
@@ -12,9 +12,9 @@ TRAIN_FILE=data/processed/sharegpt/sharegpt_data.jsonl
 STEP_NAME=""
 EPOCH_NAME=""
 
-MODEL_NAME=Llama-2-7b-hf-sharegpt-KMenasRandomDeita-1024-005-lora-epoch_4
+MODEL_NAME=Llama-2-7b-hf-sharegpt-KMeansCentroidDeita-1024-005-lora-epoch_4
 
-for peft in simonycl/data-selection-Llama-2-7b-sharegpt-KMenasRandomDeita-1024-0.05-lora-epoch_4
+for peft in simonycl/data-selection-Llama-2-7b-sharegpt-KMeansCentroidDeita-0.05-lora-epoch-4
 do
     PEFT_PATH=$peft
     # extract the model name from split string by delimiter '-' and get the third last element
@@ -22,13 +22,13 @@ do
         --lora_model_name_or_path $PEFT_PATH \
         --base_model_name_or_path $MODEL_NAME_OR_PATH \
         --tokenizer_name_or_path $MODEL_NAME_OR_PATH \
-        --output_dir /mnt/data/data-selection/output/data_selection_${MODEL_NAME} \
+        --output_dir /mnt/ceph_rbd/data-selection/output/data_selection_${MODEL_NAME} \
         --save_tokenizer \
         --push_to_hub_id simonycl/data_selection_${MODEL_NAME}
 done
 
-cd /mnt/data/lm-evaluation-harness
-bash eval_model.sh /mnt/data/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/data/EasyLM/eval_results/data_selection_$MODEL_NAME.log
+cd /mnt/ceph_rbd/lm-evaluation-harness
+bash eval_model.sh /mnt/ceph_rbd/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/ceph_rbd/EasyLM/eval_results/data_selection_$MODEL_NAME.log
 
-cd /mnt/data/data-selection/
-bash scripts/eval/cohere.sh /mnt/data/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/data/EasyLM/eval_results/data_selection_$MODEL_NAME-mmlu.log
+cd /mnt/ceph_rbd/data-selection/
+bash scripts/eval/cohere.sh /mnt/ceph_rbd/data-selection/output/data_selection_${MODEL_NAME} data_selection_$MODEL_NAME > /mnt/ceph_rbd/EasyLM/eval_results/data_selection_$MODEL_NAME-mmlu.log
