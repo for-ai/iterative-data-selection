2d295e22a73832aed8053d6e8d6193aacc299c5a
Simon <simon011130@gmail.com>
2024-03-11 14:35:50 +0000

    add new scripts

--- scripts/finetune_llama_with_accelerate_wizardlm+sharegpt_1.sh
+++ scripts/finetune_llama_with_accelerate_wizardlm+sharegpt_1.sh
@@ -67,10 +67,10 @@ do
 
     mkdir -p eval_results
     # Run evaluation on ARC, GSM8K, HellaSwag, TruthfulQA, and TrivialQA
-    bash lm-evaluation-harness/eval_model.sh output/data_selection_${MODEL_NAME}_lora_merged/ data_selection_$MODEL_NAME > eval_results/data_selection_$MODEL_NAME.log
+    bash lm-evaluation-harness/eval_model.sh output/data_selection_${DATASET}_${MODEL_NAME}_lora_merged/ data_selection_$MODEL_NAME > eval_results/data_selection_$MODEL_NAME.log
 
     # Evaluation script for MMLU, TydiQA and CodeX-HumanEval
-    bash scripts/eval/mmlu.sh output/data_selection_${MODEL_NAME}_lora_merged/ data_selection_$MODEL_NAME > eval_results/data_selection_$MODEL_NAME-mmlu.log
+    bash scripts/eval/mmlu.sh output/data_selection_${DATASET}_${MODEL_NAME}_lora_merged/ data_selection_$MODEL_NAME > eval_results/data_selection_$MODEL_NAME-mmlu.log
 done
 
 # nohup bash scripts/finetune_llama_with_accelerate.sh > logs/finetune_with_accelerate_Llama-2-7b-hf-sharegpt_lora_1.log 2>&1 &
